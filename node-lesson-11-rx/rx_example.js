315622368c75e361941c276ee06f858c6309b637
Denys Tkachuk <datkach77@gmail.com>
2024-11-14 10:52:35 +0200

    #hwnode11 updated .subscribe

--- node-lesson-11-rx/rx_example.js
+++ node-lesson-11-rx/rx_example.js
@@ -2,17 +2,17 @@
 // В результаті .subscribe має викликатися один раз
 
 import { of, interval } from 'rxjs';
-import { mergeMap, take, delay } from 'rxjs/operators';
+import { mergeMap, take } from 'rxjs/operators';
 const firstObservable = of('Перший потік');
 const secondObservable = () => interval(1000).pipe(take(3));
 // Вкладений subscribe
-firstObservable.subscribe(data1 => {
-    // secondObservable().subscribe(data2 => {
-    //     console.log('Вкладений subscribe:', data2);
-    // });
+// firstObservable.subscribe(data1 => {
+//     // secondObservable().subscribe(data2 => {
+//     //     console.log('Вкладений subscribe:', data2);
+//     // });
+//     secondObservable().pipe( mergeMap(data2 => of(`Вкладений subscribe new: ${data2}`)) )
+//         .forEach(res => console.log(res));
+// });
 
-    secondObservable().pipe(
-        mergeMap(data2 => of(`Вкладений subscribe new: ${data2}`))
-    ).forEach(res => console.log(res));
-
-});
+firstObservable.pipe( mergeMap(() => secondObservable()) )
+    .subscribe(data2 => { console.log('Вкладений subscribe new:', data2); });
