46638d435c0c611b4a1715cce0c5bf49858eb26e
Denys Tkachuk <datkach77@gmail.com>
2024-11-18 15:00:55 +0200

    #hwnode12 added generator-like method

---
+++ node-lesson-12-async/generator.js
@@ -0,0 +1,37 @@
+// Задача реалізувати createGenerator яка буде мати аутпут 1, 2, 3, undefined.
+// createGenerator є схожим на function* generator()
+
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const generator1 = generator();
+
+console.log(generator1.next().value);
+console.log(generator1.next().value);
+console.log(generator1.next().value);
+console.log(generator1.next().value);
+
+function createGenerator() {
+    let count = 1;
+    return function () {
+        if (count <= 3) {
+            // Ваш код тут
+            return count++;
+        } else {
+            // Ваш код тут
+        }
+    };
+}
+
+const generator2 = createGenerator();
+
+console.log(generator2()); // 1
+console.log(generator2()); // 2
+console.log(generator2()); // 3
+console.log(generator2()); // undefined
+
+// console.log(createGenerator().call());
+// console.log(createGenerator().call());
