38fd82392c819792529787f2788bdd02ec7b5983
Denys Tkachuk <datkach77@gmail.com>
2024-12-07 20:07:24 +0200

    #nodetermproject created initial docker files

---
+++ term-project/init.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    post VARCHAR(1024) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+--CREATE TABLE IF NOT EXISTS tokens (
+--    id SERIAL PRIMARY KEY,
+--    user_id INT NOT NULL,
+--    access_token TEXT NOT NULL,
+--    refresh_token TEXT NOT NULL,
+--    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+--);
