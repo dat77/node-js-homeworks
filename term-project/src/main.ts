856acf539db9c6c3dccb9ab030a873e56649538a
Denys Tkachuk <datkach77@gmail.com>
2024-12-09 11:23:03 +0200

    #nodetermproject added swagger api documentation

--- term-project/src/main.ts
+++ term-project/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('Node.js Term project')
+      .setDescription('This is Rest API server on NestJS framework')
+      .setVersion('1.0')
+      .addTag('NodeNestTermProject')
+      .build();
+  const swaggerDocumentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, swaggerDocumentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
